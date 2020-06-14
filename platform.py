01698f85ad1143f041c5dbf3209c5b8b0391a82d
miceuz <mic@hardcore.lt>
2020-06-14 17:13:21 +0300

    Add a missing indent

--- platform.py
+++ platform.py
@@ -64,7 +64,7 @@ class AtmelsamPlatform(PlatformBase):
             if not board.get("build.mcu", "").startswith("samd"):
                 self.packages["framework-arduino-sam"]["optional"] = True
             if framework_package in self.packages:
-            self.packages[framework_package]["optional"] = False
+                self.packages[framework_package]["optional"] = False
             self.packages["framework-cmsis"]["optional"] = False
             self.packages["framework-cmsis-atmel"]["optional"] = False
             if build_core in ("sodaq", "tuino0", "reprap"):
